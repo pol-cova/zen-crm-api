511458edcc6be3027a69d9b19c7a75e029d68a5e
pol-cova <83374367+pol-cova@users.noreply.github.com>
2025-03-23 22:25:52 -0600

    prepare for prod

--- core/settings.py
+++ core/settings.py
@@ -200,28 +200,3 @@ CORS_ALLOW_HEADERS = [
     'x-requested-with',
 ]
 """
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
-            'style': '{',
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/django-errors.log'),
-            'formatter': 'verbose',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
-}
